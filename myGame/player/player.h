82618b20561672c946a914c58d7d696fc1d7a5c6
Costa <117647243+Jaca7x@users.noreply.github.com>
2025-05-26 23:10:04 -0300

    Update player.h
    adicionado variaveis para o pulo, andar de costa etc...

--- myGame/player/player.h
+++ myGame/player/player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+
 #include "../librays/raylib.h" //Biblioteca raylib
 
 typedef struct player //struct player
@@ -8,6 +9,7 @@ typedef struct player //struct player
     Vector2 position; //posição X e Y
     float speedWalk; //velocidade de caminhada
     float speedRun; //velocidade de corrida
+    float speedWalkBack; //velocidade andando pra trás
     int currentFrame; //frame atual
     int frameCounter; //contador de frames
     int frameWidth; //largura do frame
@@ -15,12 +17,19 @@ typedef struct player //struct player
     Texture2D spritePlayerRun; //textura da corrida
     Texture2D spritePlayerWalk; //textura da caminhada
     Texture2D spritePlayerIdle; //textura quando está parado
+    Texture2D spritePlayerJump; //textura quando está pulando
     bool isRunning; //está correndo?
     bool isMoving; //está se movendo?
+    bool isJumping; //está pulando?
     float stamina; //estamina 
     int frameRun; //quantidade de frames da textura de corrida
     int frameWalk; //quantidade de frames da textura de caminhada
     int frameIdle; //quantidade de frames da textura de ficar parado
+    int frameJump; //quantidade de frames da textura de pular
+    float groundY; //posição y do chão
+    float gravity; //gravidade
+    float velocityY; //velocidade vertical do player
+    float direction;
 } Player;
 
 void InitPlayer(Player *player); //Func para inicializar o player
