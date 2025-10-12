1be6024d073bbd9f200a9737b3f6498484c2d1f0
Jaca7x <eduardo.costa.miguel@escola.pr.gov.br>
2025-10-12 02:20:19 -0300

    adicionando botões de interações, funções para checar a interação, exclamação de dica

--- npcs/ghost.c
+++ npcs/ghost.c
@@ -1,13 +1,21 @@
 #include "ghost.h"
 
+bool InteractionWithGhost(const Ghost *ghost, Player *player)
+{
+    float dx = player->position.x - ghost->position.x;
+    return (dx > -150 && dx < 60);
+}
+
 void InitGhost(Ghost *ghost) 
 {
     ghost->position = (Vector2){800, 540}; 
     ghost->ghostIdle = LoadTexture("resources/sprites/npc/ghost-idle.png"); 
     ghost->ghostBtnE = LoadTexture("resources/sprites/btns/btn-E.png");
     ghost->ghostSppech = LoadTexture("resources/sprites/npc/ghost-speech.png");
+    ghost->ghostExclamation = LoadTexture("resources/sprites/npc/exclamation.png");
 
     ghost->frameIdle = 3;
+    ghost->frameExclamation = 2;
 
     ghost->frameWidth = ghost->ghostIdle.width / ghost->frameIdle; 
     ghost->frameHeight = ghost->ghostIdle.height; 
@@ -25,7 +33,7 @@ void UpdateGhost(Ghost *ghost, float delta)
     }
 }
 
-void DrawGhost(const Ghost *ghost) 
+void DrawGhost(Ghost *ghost, Player *player) 
 {
     Rectangle source = {
         ghost->currentFrame * ghost->frameWidth,
@@ -36,12 +44,21 @@ void DrawGhost(const Ghost *ghost)
     Rectangle dest = {
         ghost->position.x,
         ghost->position.y,
-        ghost->frameWidth /2.5,
+        ghost->frameWidth / 2.5,
         ghost->frameHeight / 2.5
     };
     Vector2 origin = {0, 0};
 
     DrawTexturePro(ghost->ghostIdle, source, dest, origin, 0.0f, Fade(WHITE, 0.7f));
+
+    if (InteractionWithGhost(ghost, player))
+    {
+        DrawTexture(ghost->ghostBtnE, ghost->position.x + 15, ghost->position.y - 35, WHITE);
+    }
+    else
+    {
+        DrawTexture(ghost->ghostExclamation, ghost->position.x + 25, ghost->position.y - 25, WHITE);
+    }
 }
 
 void UnloadGhost(Ghost *ghost) 
