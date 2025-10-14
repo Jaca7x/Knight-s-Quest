c85023a77ebb14d80b1fbde3987378145f732c80
Jaca7x <eduardo.costa.miguel@escola.pr.gov.br>
2025-10-13 21:05:36 -0300

    enum das falas

--- npcs/ghost.h
+++ npcs/ghost.h
@@ -4,10 +4,21 @@
 #include "librays/raylib.h"
 #include "player/player.h"
 
+
+typedef enum {
+    DIALOG_CLOSED_GHOST,
+    DIALOG_PLAYER_GHOST_TALKING,
+    DIALOG_GHOST_TALKING,
+    DIALOG_PLAYER_GHOST_TALKING2,
+    DIALOG_GHOST_TALKING2,
+    DIALOG_PLAYER_GHOST_TALKING3,
+} DialogStateGhost;
+
+
 // Ghost NPC structure
 typedef struct ghost {
     Vector2 position;
-    Texture2D ghostSppech;
+    Texture2D ghostSpeech;
     Texture2D ghostIdle;
     Texture2D ghostBtnE;
     Texture2D ghostExclamation;
@@ -18,11 +29,13 @@ typedef struct ghost {
     int frameHeight;  
     int currentFrame; 
     int frameCounter; 
+
+    Font textFont; 
 } Ghost;
 
 void InitGhost(Ghost *ghost);
-void UpdateGhost(Ghost *ghost, float delta);
-void DrawGhost(Ghost *ghost, Player *player);
+void UpdateGhost(Ghost *ghost, Player *player, float delta, DialogStateGhost *dialogStateGhost, float *dialogoTimer);
+void DrawGhost(Ghost *ghost, Player *player, DialogStateGhost dialogStateGhost);
 void UnloadGhost(Ghost *ghost);
 
 #endif // GHOST_H
